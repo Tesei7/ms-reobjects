9dd1cfe9f6b71da3e78e6c0c2a6b458e10d3c759
Ilia Bochkarev <ilia_bochkarev@epam.com>
2018-04-05 19:22:08 +0300

    Feign client

--- src/main/java/tesei7/ms/reobjects/agent/AgentsClient.java
+++ src/main/java/tesei7/ms/reobjects/agent/AgentsClient.java
@@ -1,6 +1,7 @@
 package tesei7.ms.reobjects.agent;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +10,7 @@ import java.util.List;
 
 @FeignClient("agents")
 public interface AgentsClient {
-    @RequestMapping(
-            method= RequestMethod.GET,
-            value="/api/v1/agents/search/findByZipCode?zipCode={zipCode}",
-            consumes="application/json")
-    List<Agent> findAgentsByZipcode(@PathVariable("zipCode") String zipCode);
+    @RequestMapping(method = RequestMethod.GET,
+            value = "/api/v1/agents/search/findByZipCode?zipCode={zipCode}", consumes = "application/json")
+    Resources<Agent> findAgentsByZipcode(@PathVariable("zipCode") String zipCode);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO address (id, zip_code, city, address) VALUES (1, '90210', 'Beverly Hills, CA', '2241 Bowmont Dr');
-INSERT INTO address (id, zip_code, city, address) VALUES (2, '90211', 'Beverly Hills, CA', '265 S La Peer Dr');
-INSERT INTO address (id, zip_code, city, address) VALUES (3, '90016', 'Los Angeles, CA', '2314 S Marvin Ave');
+INSERT INTO address (id, zip_code, city, address) VALUES (1, '90210', 'Beverly Hills, CA', '2214 Bowmont Dr');
+INSERT INTO address (id, zip_code, city, address) VALUES (2, '90211', 'Beverly Hills, CA', '256 S La Peer Dr');
+INSERT INTO address (id, zip_code, city, address) VALUES (3, '90016', 'Los Angeles, CA', '2341 S Marvin Ave');
 
 INSERT INTO RE_OBJECTS (id, address_id, type, area, price) VALUES (1, 1, 'FOUR_BEDROOM', 3558, 4250000);
 INSERT INTO RE_OBJECTS (id, address_id, type, area, price) VALUES (2, 2, 'FOUR_BEDROOM', 1977, 2399000);
